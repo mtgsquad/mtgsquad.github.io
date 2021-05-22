185749f38bd198a52d9224d32caa66067961cd91
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-05-22 21:45:24 +0500

    Add offline functionality!

--- service-worker.js
+++ service-worker.js
@@ -4,4 +4,41 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.NetworkFirst()
-)
+);
+
+const version = "0.6.18";
+const cacheName = `devlog-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/style.css`,
+        `/theme.js`,
+        `/manifest.json`,
+        `/main.js`,
+        `/logo.png`,
+        `/donations/cancel.html`,
+        `/donations/thanks.html`,
+        `/admin/new.html`,
+        `/admin/newPost.js`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
